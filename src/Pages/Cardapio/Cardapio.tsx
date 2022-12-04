95cdfd2d48e2d7780a686c3646cc76ca96e69534
Rubendvb <rubenvasquez556@gmail.com>
2022-12-03 23:27:49 -0300

    Criando componente Filtro

--- src/Pages/Cardapio/Cardapio.tsx
+++ src/Pages/Cardapio/Cardapio.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Buscador from "../../components/Buscador/Buscador";
+import Filtros from "../../components/Filtros/Filtros";
 import styles from "./Cardapio.module.scss";
 import logo from "/src/assets/Img/Logo/logo.svg";
 
@@ -20,6 +21,10 @@ export default function Cardapio() {
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
 
         <Buscador busca={busca} setBusca={setBusca} />
+
+        <div className={styles.cardapio__filtros}>
+          <Filtros />
+        </div>
       </section>
     </main>
   );
