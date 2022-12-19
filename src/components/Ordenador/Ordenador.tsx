59fdb45a385744a3da7bd7fde691fee302da075e
Rubendvb <rubenvasquez556@gmail.com>
2022-12-19 00:33:01 -0300

    Criando lógica do dropdown

--- src/components/Ordenador/Ordenador.tsx
+++ src/components/Ordenador/Ordenador.tsx
@@ -1,12 +1,26 @@
 import styles from "./Ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import { useState } from "react";
+import classNames from "classnames";
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
 export default function Ordenador() {
-  return (
-    <button className={styles.ordenador}>
-      <span>Ordenar Por</span>
+  const [aberto, setAberto] = useState(false);
 
-      <div className={styles.ordenador__options}>
+  return (
+    <button
+      className={styles.ordenador}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)} // Ao clicar por fora o dropdown fecha
+    >
+      <span>Ordenar Por</span>
+      {aberto ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: aberto,
+        })}
+      >
         {opcoes.map((opcao) => (
           <div key={opcao.value} className={styles.ordenador}>
             {opcao.nome}
