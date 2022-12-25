91de048502d27fdf9ee2796957506792a6135b5f
Rubendvb <rubenvasquez556@gmail.com>
2022-12-25 17:02:04 -0300

    Criando Inicio

--- src/Pages/Inicio/Inicio.tsx
+++ src/Pages/Inicio/Inicio.tsx
@@ -1,7 +1,29 @@
+import cardapio from "../../data/cardapio.json";
+
+import "./Inicio.scss";
+
 export default function Inicio() {
+  let pratosRecomendados = [...cardapio];
+
+  pratosRecomendados = pratosRecomendados
+    .sort(() => 0.5 - Math.random())
+    .splice(0, 3);
+
   return (
     <section>
-      <h3>Recomendações da cozinha</h3>
+      <h3 className="titulo">Recomendações da cozinha</h3>
+      <div className="recomendados">
+        {pratosRecomendados.map((item) => {
+          return (
+            <div key={item.id} className="recomendado">
+              <div className="recomendado__imagem">
+                <img src={item.photo} alt={item.title} />
+              </div>
+              <button className="recomendado__botao">Ver mais</button>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import Cardapio from "./Pages/Cardapio/Cardapio";
 
 import "normalize.css";
 import "./assets/styles/main.css";
+import Inicio from "./Pages/Inicio/Inicio";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Cardapio />
+    {/* <Cardapio /> */}
+    <Inicio />
   </React.StrictMode>
 );
