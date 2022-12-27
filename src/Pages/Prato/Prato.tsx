cc614840020d79affb70cb90d78e9c6ccb234e34
Rubendvb <rubenvasquez556@gmail.com>
2022-12-27 05:32:21 -0300

    resolvendo header na pagina notfound

--- src/Pages/Prato/Prato.tsx
+++ src/Pages/Prato/Prato.tsx
@@ -1,9 +1,11 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import "./Prato.scss";
 import cardapio from "../../data/cardapio.json";
 import TagsPrato from "../../components/TagsPrato/TagsPrato";
 import NotFound from "../NotFound/NotFound";
+import Header from "../../components/Header/Header";
 
 export default function Prato() {
   const { id } = useParams();
@@ -17,24 +19,33 @@ export default function Prato() {
   }
 
   return (
-    <>
-      <button className="voltar" onClick={() => navigate(-1)}>
-        {"< Voltar"}
-      </button>
+    <Routes>
+      <Route path="*" element={<Header />}>
+        <Route
+          index
+          element={
+            <>
+              <button className="voltar" onClick={() => navigate(-1)}>
+                {"< Voltar"}
+              </button>
 
-      <section className="container">
-        <h1 className="titulo">{prato.title}</h1>
+              <section className="container">
+                <h1 className="titulo">{prato.title}</h1>
 
-        <div className="imagem">
-          <img src={prato.photo} alt={prato.title} />
-        </div>
+                <div className="imagem">
+                  <img src={prato.photo} alt={prato.title} />
+                </div>
 
-        <div className="conteudo">
-          <p className="conteudo__descricao">{prato.description}</p>
+                <div className="conteudo">
+                  <p className="conteudo__descricao">{prato.description}</p>
 
-          <TagsPrato {...prato} />
-        </div>
-      </section>
-    </>
+                  <TagsPrato {...prato} />
+                </div>
+              </section>
+            </>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
